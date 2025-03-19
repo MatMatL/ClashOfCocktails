c3cf08710b4cb444f9890afa3075add88d50f67c
Gabypic <gpicard77350@gmail.com>
2025-03-19 14:04:47 +0100

    cocktails list road

--- routes.js
+++ routes.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const fs = require("fs")
+const path = require("path")
+
 const router = express.Router();
 
 router.get("/api/cocktail", (req, res) => {
@@ -6,6 +9,26 @@ router.get("/api/cocktail", (req, res) => {
         name: "Elixir de Rage",
         ingredients: ["Rhum", "Jus de grenade", "Sirop de sucre"],
         description: "Un cocktail puissant inspiré du sort de Rage dans Clash of Clans !",
+    });
+});
+
+router.get("/api/cocktails_list", (req , res) => {
+    const filePath = path.join(__dirname, "Cocktails/BlacksTroups.json")
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            console.error("Erreur de lecture du fichier JSON :", err);
+            return res.status(500).json({error: "Erreur interne du serveur"});
+        }
+
+        try {
+            const jsonData = JSON.parse(data);
+            const nomsCocktails = jsonData.troupes_noires.map(troupe => troupe.cocktail);
+            res.json({cocktails: nomsCocktails});
+        } catch (parseError) {
+            console.error("Erreur d'analyse du JSON :", parseError);
+            res.status(500).json({error: "Erreur interne du serveur"});
+        }
     });
 });
 
