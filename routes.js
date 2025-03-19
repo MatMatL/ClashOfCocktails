000e2b571b4433a0bf89d2a6914a950b02d2c832
Gabypic <gpicard77350@gmail.com>
2025-03-19 14:23:27 +0100

    modif route liste cocktails avec tous les cocktails + affichage dans index

--- routes.js
+++ routes.js
@@ -4,6 +4,23 @@ const path = require("path")
 
 const router = express.Router();
 
+const readJsonFile = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                try {
+                    resolve(JSON.parse(data));
+                } catch (parseError) {
+                    reject(parseError);
+                }
+            }
+        });
+    });
+};
+
+
 router.get("/api/cocktail", (req, res) => {
     res.json({
         name: "Elixir de Rage",
@@ -12,24 +29,26 @@ router.get("/api/cocktail", (req, res) => {
     });
 });
 
-router.get("/api/cocktails_list", (req , res) => {
-    const filePath = path.join(__dirname, "Cocktails/BlacksTroups.json")
+router.get("/api/cocktails_list", async (req, res) => {
+    try {
+        const files = ["Cocktails/BlacksTroups.json", "Cocktails/Heros.json", "Cocktails/Troups.json"];
+        const filePaths = files.map(file => path.join(__dirname, file));
 
-    fs.readFile(filePath, "utf8", (err, data) => {
-        if (err) {
-            console.error("Erreur de lecture du fichier JSON :", err);
-            return res.status(500).json({error: "Erreur interne du serveur"});
-        }
+        // Lire les fichiers JSON en parallèle
+        const jsonDataArray = await Promise.all(filePaths.map(readJsonFile));
 
-        try {
-            const jsonData = JSON.parse(data);
-            const nomsCocktails = jsonData.troupes_noires.map(troupe => troupe.cocktail);
-            res.json({cocktails: nomsCocktails});
-        } catch (parseError) {
-            console.error("Erreur d'analyse du JSON :", parseError);
-            res.status(500).json({error: "Erreur interne du serveur"});
-        }
-    });
+        // Extraire les noms des cocktails de chaque fichier
+        const allCocktails = jsonDataArray.flatMap(jsonData => {
+            return jsonData.troupes_noires?.map(troupe => troupe.cocktail) ||
+                jsonData.heros?.map(hero => hero.cocktail) ||
+                jsonData.troupes?.map(troupe => troupe.cocktail) || [];
+        });
+
+        res.json({ cocktails: allCocktails });
+    } catch (error) {
+        console.error("Erreur lors de la lecture des fichiers JSON :", error);
+        res.status(500).json({ error: "Erreur interne du serveur" });
+    }
 });
 
 module.exports = router;
